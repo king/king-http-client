0dd7b2ba9fbf7cc08f6938d0f0167ee3503c18ff
Magnus Gustafsson <magnus.gustafsson@king.com>
2016-09-26 17:23:42 +0200

    Update missing copyrights

--- client/src/main/java/com/king/platform/net/http/netty/eventbus/EventListener.java
+++ client/src/main/java/com/king/platform/net/http/netty/eventbus/EventListener.java
@@ -1,3 +1,8 @@
+// Copyright (C) king.com Ltd 2016
+// https://github.com/king/king-http-client
+// Author: Magnus Gustafsson
+// License: Apache 2.0, https://raw.github.com/king/king-http-client/LICENSE-APACHE
+
 package com.king.platform.net.http.netty.eventbus;
 
 
