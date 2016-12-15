ed1ffaa891b1d90a1ee7dc361000a94845ea2651
Magnus Gustafsson <magnus.gustafsson@king.com>
2016-12-15 09:25:59 +0100

    Added test for CustomHttpBodyBuilder

---
+++ client/src/test/java/com/king/platform/net/http/netty/requestbuilder/CustomHttpBodyBuilderTest.java
@@ -0,0 +1,25 @@
+// Copyright (C) king.com Ltd 2016
+// https://github.com/king/king-http-client
+// Author: Magnus Gustafsson
+// License: Apache 2.0, https://raw.github.com/king/king-http-client/LICENSE-APACHE
+
+package com.king.platform.net.http.netty.requestbuilder;
+
+import com.king.platform.net.http.netty.request.HttpBody;
+import org.junit.Test;
+
+import java.nio.charset.Charset;
+
+import static org.junit.Assert.*;
+import static se.mockachino.Mockachino.mock;
+
+
+public class CustomHttpBodyBuilderTest {
+	@Test
+	public void createHttpBodyShouldReturnProvidedBody() throws Exception {
+		HttpBody httpBody = mock(HttpBody.class);
+		CustomHttpBodyBuilder customHttpBodyBuilder = new CustomHttpBodyBuilder(httpBody);
+		assertSame(httpBody, customHttpBodyBuilder.createHttpBody("contentType", Charset.defaultCharset(), false));
+
+	}
+}
