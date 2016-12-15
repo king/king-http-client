38da86fd8a28940ace34f53ba70852249df2b2ac
Magnus Gustafsson <magnus.gustafsson@king.com>
2016-12-15 10:19:34 +0100

    Added javadoc for BackPressure

--- client/src/main/java/com/king/platform/net/http/netty/backpressure/BackPressure.java
+++ client/src/main/java/com/king/platform/net/http/netty/backpressure/BackPressure.java
@@ -8,8 +8,18 @@ package com.king.platform.net.http.netty.backpressure;
 import com.king.platform.net.http.netty.ServerInfo;
 
 public interface BackPressure {
+	/**
+	 * This will be called for each created request
+	 * If this returns false, an ERROR will be triggered and releaseSlot will be called directly for the same serverInfo.
+	 * @param serverInfo the server the request is created against
+	 * @return true if the connection should be allowed
+	 */
 	boolean acquireSlot(ServerInfo serverInfo);
 
+	/**
+	 * This will be called for COMPLETED and ERROR events on each request
+	 * @param serverInfo the server the request was created against
+	 */
 	void releaseSlot(ServerInfo serverInfo);
 
 }
