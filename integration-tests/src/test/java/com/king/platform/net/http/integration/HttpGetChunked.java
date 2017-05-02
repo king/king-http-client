de6d41261fcf7285c65e55eb7e75e7aaba2ca172
Magnus Gustafsson <magnus.gustafsson@king.com>
2017-05-02 10:44:30 +0200

    Added some tests

---
+++ integration-tests/src/test/java/com/king/platform/net/http/integration/HttpGetChunked.java
@@ -0,0 +1,82 @@
+// Copyright (C) king.com Ltd 2016
+// https://github.com/king/king-http-client
+// Author: Magnus Gustafsson
+// License: Apache 2.0, https://raw.github.com/king/king-http-client/LICENSE-APACHE
+
+package com.king.platform.net.http.integration;
+
+
+import com.king.platform.net.http.HttpClient;
+import com.king.platform.net.http.HttpResponse;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class HttpGetChunked {
+
+	private static final String message1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+	private static final String message2 = "Aenean ut leo elit.";
+	private static final String message3 = "Aenean rutrum tincidunt arcu, lacinia scelerisque.";
+
+	private int port;
+	private HttpClient httpClient;
+	private Server server;
+
+	@Before
+	public void setUp() throws Exception {
+		port = JettyIntegrationServer.findFreePort();
+
+		server = new Server(port);
+		server.setHandler(new MyHandler());
+		server.getBean(ServerConnector.class).setIdleTimeout(2000);
+
+		server.start();
+
+		httpClient = new TestingHttpClientFactory().create();
+		httpClient.start();
+	}
+
+	@Test
+	public void getChunked() throws Exception {
+		HttpResponse<String> httpResponse = httpClient.createGet("http://localhost:" + port).keepAlive(true).build().execute().get().getHttpResponse();
+		String body = httpResponse.getBody();
+		assertEquals(message1 +"\r\n"+ message2 +"\r\n"+ message3 +"\r\n", body);
+		String header = httpResponse.getHeader("Transfer-Encoding");
+		assertEquals("chunked", header);
+	}
+
+
+	@After
+	public void tearDown() throws Exception {
+		httpClient.shutdown();
+		server.stop();
+	}
+
+	public static class MyHandler extends AbstractHandler {
+		public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+			baseRequest.setHandled(true);
+			response.setContentType("text/plain; charset=utf-8");
+			response.setStatus(HttpServletResponse.SC_OK);
+
+			ServletOutputStream out = response.getOutputStream();
+
+			out.println(message1);
+			out.flush();
+			out.println(message2);
+			out.flush();
+			out.println(message3);
+		}
+	}
+}
--- integration-tests/src/test/java/com/king/platform/net/http/integration/HttpPostWithInputStreamBody.java
+++ integration-tests/src/test/java/com/king/platform/net/http/integration/HttpPostWithInputStreamBody.java
@@ -7,6 +7,11 @@ package com.king.platform.net.http.integration;
 
 
 import com.king.platform.net.http.HttpClient;
+import com.king.platform.net.http.netty.request.HttpBody;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelProgressivePromise;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,10 +20,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicReference;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class HttpPostWithInputStreamBody {
@@ -27,10 +35,13 @@ public class HttpPostWithInputStreamBody {
 	private int port;
 
 	private String okBody = "EVERYTHING IS OKAY!";
-	private String content = "BODY CONTENT";
+	private byte[] content;
 
 	@Before
 	public void setUp() throws Exception {
+		content = new byte[1024 * 16];
+		new Random().nextBytes(content);
+
 		integrationServer = new JettyIntegrationServer();
 		integrationServer.start();
 		port = integrationServer.getPort();
@@ -43,52 +54,25 @@ public class HttpPostWithInputStreamBody {
 
 	@Test
 	public void postBody() throws Exception {
-		final AtomicReference<String> bodyContent = new AtomicReference<>();
+		final AtomicReference<byte[]> bodyContent = new AtomicReference<>();
 		integrationServer.addServlet(new HttpServlet() {
 			@Override
 			protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 				byte[] body = readPostBody(req);
-				bodyContent.set(new String(body));
-
+				bodyContent.set(body);
 				resp.getWriter().write(okBody);
 				resp.getWriter().flush();
 			}
 		}, "/testOk");
 
 		BlockingHttpCallback httpCallback = new BlockingHttpCallback();
-		httpClient.createPost("http://localhost:" + port + "/testOk").content(content.getBytes()).build().execute(httpCallback);
+		httpClient.createPost("http://localhost:" + port + "/testOk").content(content).build().execute(httpCallback);
 
 		httpCallback.waitForCompletion();
 
-		assertEquals(content, bodyContent.get());
+		assertArrayEquals(content, bodyContent.get());
 		assertEquals(200, httpCallback.getStatusCode());
 
-
-	}
-
-	@Test
-	public void postBodyWithContentType() throws Exception {
-		final AtomicReference<String> contentTypeValue = new AtomicReference<>();
-		integrationServer.addServlet(new HttpServlet() {
-			@Override
-			protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-				contentTypeValue.set(req.getContentType());
-
-				resp.getWriter().write(okBody);
-				resp.getWriter().flush();
-			}
-		}, "/testOk");
-
-		BlockingHttpCallback httpCallback = new BlockingHttpCallback();
-		String contentType = "text/unit test";
-
-		httpClient.createPost("http://localhost:" + port + "/testOk").content(content.getBytes()).contentType(contentType).build().execute(httpCallback);
-
-		httpCallback.waitForCompletion();
-
-		assertEquals(okBody, httpCallback.getBody());
-		assertEquals(200, httpCallback.getStatusCode());
-		assertEquals(contentType, contentTypeValue.get());
 
 	}
 
@@ -110,6 +94,119 @@ public class HttpPostWithInputStreamBody {
 
 	}
 
+	@Test
+	public void postBodyAsStream() throws Exception {
+		final AtomicReference<byte[]> bodyContent = new AtomicReference<>();
+		integrationServer.addServlet(new HttpServlet() {
+			@Override
+			protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+				byte[] body = readPostBody(req);
+				bodyContent.set(body);
+
+				resp.getWriter().write(okBody);
+				resp.getWriter().flush();
+			}
+		}, "/testOk");
+
+		BlockingHttpCallback httpCallback = new BlockingHttpCallback();
+		httpClient.createPost("http://localhost:" + port + "/testOk").content(new ByteArrayInputStream(content)).build().execute(httpCallback);
+
+		httpCallback.waitForCompletion();
+
+		assertArrayEquals(content, bodyContent.get());
+		assertEquals(200, httpCallback.getStatusCode());
+
+	}
+
+	@Test
+	public void postBodyCustomHttpBody() throws Exception {
+		final AtomicReference<byte[]> bodyContent = new AtomicReference<>();
+		integrationServer.addServlet(new HttpServlet() {
+			@Override
+			protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+				byte[] body = readPostBody(req);
+				bodyContent.set(body);
+
+				resp.getWriter().write(okBody);
+				resp.getWriter().flush();
+			}
+		}, "/testOk");
+
+		BlockingHttpCallback httpCallback = new BlockingHttpCallback();
+		httpClient.createPost("http://localhost:" + port + "/testOk").content(new HttpBody() {
+			@Override
+			public long getContentLength() {
+				return content.length;
+			}
+
+			@Override
+			public String getContentType() {
+				return "application/binary";
+			}
+
+			@Override
+			public ChannelFuture writeContent(final ChannelHandlerContext ctx) throws IOException {
+				final ChannelProgressivePromise promise = ctx.newProgressivePromise();
+				promise.setProgress(0, content.length);
+				new Thread(new Runnable() {
+					@Override
+					public void run() {
+						int index = 0;
+						int length = 1024;
+						while (true) {
+							ByteBuf byteBuf = ctx.alloc().buffer(length).writeBytes(content, index, length);
+							ctx.writeAndFlush(byteBuf).awaitUninterruptibly();
+							index += length;
+							if (index >= content.length) {
+								break;
+							}
+							try {
+								Thread.sleep(100);
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+							promise.setProgress(index, content.length);
+						}
+
+						promise.setSuccess();
+					}
+				}).start();
+
+				return promise;
+			}
+		}).build().execute(httpCallback);
+
+		httpCallback.waitForCompletion();
+
+		assertArrayEquals(content, bodyContent.get());
+		assertEquals(200, httpCallback.getStatusCode());
+	}
+
+	@Test
+	public void postBodyWithContentType() throws Exception {
+		final AtomicReference<String> contentTypeValue = new AtomicReference<>();
+		integrationServer.addServlet(new HttpServlet() {
+			@Override
+			protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+				contentTypeValue.set(req.getContentType());
+
+				resp.getWriter().write(okBody);
+				resp.getWriter().flush();
+			}
+		}, "/testOk");
+
+		BlockingHttpCallback httpCallback = new BlockingHttpCallback();
+		String contentType = "text/unit test";
+
+		httpClient.createPost("http://localhost:" + port + "/testOk").content(content).contentType(contentType).build().execute(httpCallback);
+
+		httpCallback.waitForCompletion();
+
+		assertEquals(okBody, httpCallback.getBody());
+		assertEquals(200, httpCallback.getStatusCode());
+		assertEquals(contentType, contentTypeValue.get());
+
+	}
 
 	@After
 	public void tearDown() throws Exception {
