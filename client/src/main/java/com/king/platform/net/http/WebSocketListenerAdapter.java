7f37b64473b3a4e5f2dd66722b17482481fdd771
Magnus Gustafsson <magnus.gustafsson@king.com>
2017-11-23 12:15:38 +0100

    Added an adapter around WebSocketListener

---
+++ client/src/main/java/com/king/platform/net/http/WebSocketListenerAdapter.java
@@ -0,0 +1,34 @@
+package com.king.platform.net.http;
+
+
+public interface WebSocketListenerAdapter extends WebSocketListener {
+	@Override
+	default void onConnect(WebSocketConnection connection) {
+
+	}
+
+	@Override
+	default void onError(Throwable throwable) {
+
+	}
+
+	@Override
+	default void onDisconnect() {
+
+	}
+
+	@Override
+	default void onCloseFrame(int code, String reason) {
+
+	}
+
+	@Override
+	default void onBinaryFrame(byte[] payload, boolean finalFragment, int rsv) {
+
+	}
+
+	@Override
+	default void onTextFrame(String payload, boolean finalFragment, int rsv) {
+
+	}
+}
