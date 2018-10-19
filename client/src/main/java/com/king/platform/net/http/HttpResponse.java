909d63eee722c6ed64190cabaeb900f16fa9ac22
Juan Ignacio Gaudio <juan.gaudio@king.com>
2018-10-19 10:45:28 +0200

    Expore more basic info in HTTP response

--- client/src/main/java/com/king/platform/net/http/HttpResponse.java
+++ client/src/main/java/com/king/platform/net/http/HttpResponse.java
@@ -7,24 +7,38 @@ package com.king.platform.net.http;
 
 
 import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
 
 import java.util.List;
 import java.util.Map;
 
 public class HttpResponse<T> {
-	private Headers headers;
-	private final int statusCode;
+
+	private final HttpVersion httpVersion;
+	private final Headers headers;
+	private final HttpResponseStatus status;
 	private final ResponseBodyConsumer<T> responseBodyConsumer;
 
 
-	public HttpResponse(int statusCode, ResponseBodyConsumer responseBodyConsumer, HttpHeaders httpHeaders) {
-		this.statusCode = statusCode;
+	public HttpResponse(HttpVersion httpVersion, HttpResponseStatus status,
+						ResponseBodyConsumer responseBodyConsumer, HttpHeaders httpHeaders) {
+		this.httpVersion = httpVersion;
+		this.status = status;
 		this.responseBodyConsumer = responseBodyConsumer;
-		headers = new Headers(httpHeaders);
+		this.headers = new Headers(httpHeaders);
+	}
+
+	public String getHttpVersion() {
+		return httpVersion.toString();
 	}
 
 	public int getStatusCode() {
-		return statusCode;
+		return status.code();
+	}
+
+	public String getStatusReason() {
+		return status.reasonPhrase();
 	}
 
 	public T getBody() {
